3735e4f0cf27c378d03b73cd97fc2d773561b49a
sgrigerc <sgrigerc@gmail.com>
2023-06-06 02:01:16 +0300

    подправка навбара

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-'
+SECRET_KEY = 'django-insecure-i1($u(s0z55uo8f$y30syk-%qn)r@a0p)mjy9h*!$tu5e2zglp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -126,6 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'sgrigerc@gmail.com'
-EMAIL_HOST_PASSWORD = '-'
+EMAIL_HOST_PASSWORD = 'azkckeggyshxwqog'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
